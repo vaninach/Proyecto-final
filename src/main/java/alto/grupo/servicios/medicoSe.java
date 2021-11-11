35349251a23ffc3d3e4580d8feaf99d63e0ca4c3
mTorres95 <miactl@gmail.com>
2021-11-10 23:32:55 -0300

    Avances en el service del medico (trabajando en las validaciones)

--- src/main/java/alto/grupo/servicios/medicoSe.java
+++ src/main/java/alto/grupo/servicios/medicoSe.java
@@ -8,10 +8,12 @@ package alto.grupo.servicios;
 import alto.grupo.entidades.medico;
 import alto.grupo.enums.Genero;
 import alto.grupo.enums.Provincia;
+import alto.grupo.errores.Errores;
 import alto.grupo.repositorios.medicoRep;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,10 +26,28 @@ public class medicoSe {
     
     @Autowired medicoRep medRep;
     
-    public void crearMedico(Integer matricula, String nombre, String apellido, Date fechaNac, Genero genero, String mail, Provincia provincia, String ciudad, String otros, String clave, String especialidad1, String especialidad2, String especialidad3, List<Integer> centrosMedicos){
+    @Transactional
+    public void crearMedico(Integer matricula, String nombre, String apellido, Date fechaNac, Genero genero, String mail, Provincia provincia, String ciudad, String otros, String clave, String especialidad1, String especialidad2, String especialidad3, List<Integer> centrosMedicos) throws Errores{
         Optional<medico> medOpt =  medRep.findById(matricula);
         
         if(!medOpt.isPresent()){
+            try {
+                // validar(matricula); Integer
+                validar(nombre);
+                validar(apellido);
+                // validar(fechaNac);  Date
+                // validar(genero);  enum
+                validar(mail);
+                // validar(provincia); enum
+                validar(ciudad);  // si usamos la API, esto va a cambiar
+                // validar(otros); Este si puede ser nulo
+                validar(clave);
+                validar(especialidad1);
+                validar(especialidad2);
+                validar(especialidad3);
+                // validar(centrosMedicos); List de integers
+                
+            // == si ninguna validacion da error, crear y persistir ==  
             medico med = new medico();
             med.setMatricula(matricula);
             med.setNombre(nombre);
@@ -43,11 +63,22 @@ public class medicoSe {
             med.setEspecialidad2(especialidad2);
             med.setEspecialidad3(especialidad3);
             med.setCentrosMedicos(centrosMedicos);
-            return med;
-        }else{
+
+            medRep.save(med);
+
+            } catch (Errores e) {
+                System.out.println(e);
+            }
             
+        }else{
+            throw new Errores("Ya existe un médico con la matricula ingresada.");
         }
-        
-        //return medOpt.get();
     }
+    
+    private void validar(String texto) throws Errores {
+        if (texto == null || texto.isEmpty()) {
+            throw new Errores("Los datos no pueden ser nulos.");
+        }
+    }
+    
 }
