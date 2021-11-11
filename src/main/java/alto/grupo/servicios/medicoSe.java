533a6527a383713adf113df12cfb9837c1d612b4
mTorres95 <miactl@gmail.com>
2021-11-11 20:40:26 -0300

    Finalizacion del service del medico

--- src/main/java/alto/grupo/servicios/medicoSe.java
+++ src/main/java/alto/grupo/servicios/medicoSe.java
@@ -27,7 +27,7 @@ public class medicoSe {
     @Autowired medicoRep medRep;
     
     @Transactional
-    public void crearMedico(Integer matricula, String nombre, String apellido, Date fechaNac, Genero genero, String mail, Provincia provincia, String ciudad, String otros, String clave, String especialidad1, String especialidad2, String especialidad3, List<Integer> centrosMedicos) throws Errores{
+    public void crear(Integer matricula, String nombre, String apellido, Date fechaNac, Genero genero, String mail, Provincia provincia, String ciudad, String otros, String clave, String especialidad1, String especialidad2, String especialidad3, List<Integer> centrosMedicos) throws Errores{
         Optional<medico> medOpt =  medRep.findById(matricula);
         
         if(!medOpt.isPresent()){
@@ -47,24 +47,24 @@ public class medicoSe {
                 validar(especialidad3);
                 // validar(centrosMedicos); List de integers
                 
-            // == si ninguna validacion da error, crear y persistir ==  
-            medico med = new medico();
-            med.setMatricula(matricula);
-            med.setNombre(nombre);
-            med.setApellido(apellido);
-            med.setFechaNac(fechaNac);
-            med.setGenero(genero);
-            med.setMail(mail);
-            med.setProvincia(provincia);
-            med.setCiudad(ciudad);
-            med.setOtros(otros);
-            med.setClave(clave);
-            med.setEspecialidad1(especialidad1);
-            med.setEspecialidad2(especialidad2);
-            med.setEspecialidad3(especialidad3);
-            med.setCentrosMedicos(centrosMedicos);
+                // == si ninguna validacion da error, crear y persistir ==  
+                medico med = new medico();
+                med.setMatricula(matricula);
+                med.setNombre(nombre);
+                med.setApellido(apellido);
+                med.setFechaNac(fechaNac);
+                med.setGenero(genero);
+                med.setMail(mail);
+                med.setProvincia(provincia);
+                med.setCiudad(ciudad);
+                med.setOtros(otros);
+                med.setClave(clave);
+                med.setEspecialidad1(especialidad1);
+                med.setEspecialidad2(especialidad2);
+                med.setEspecialidad3(especialidad3);
+                med.setCentrosMedicos(centrosMedicos);
 
-            medRep.save(med);
+                medRep.save(med);
 
             } catch (Errores e) {
                 System.out.println(e);
@@ -75,10 +75,69 @@ public class medicoSe {
         }
     }
     
-    private void validar(String texto) throws Errores {
+    public void validar(String texto) throws Errores {
         if (texto == null || texto.isEmpty()) {
             throw new Errores("Los datos no pueden ser nulos.");
         }
+    }
+    
+    public void modificar(Integer matricula, String nombre, String apellido, Date fechaNac, Genero genero, String mail, Provincia provincia, String ciudad, String otros, String clave, String especialidad1, String especialidad2, String especialidad3, List<Integer> centrosMedicos) throws Errores{
+        Optional<medico> medOpt = medRep.findById(matricula);
+        
+        // ==========================================================
+        // PROBLEMA: sin validacion => posibles cpos vacios seteados
+        // SOLUCION: pre-llenado de fields
+        // ==========================================================
+        
+        if(medOpt.isPresent()){
+            medico med = medOpt.get();
+            // if(buscarCambios(matricula))  Integer
+                med.setMatricula(matricula);
+            if(buscarCambios(nombre))
+                med.setNombre(nombre);
+            if(buscarCambios(apellido))
+                med.setApellido(apellido);
+            // if(buscarCambios(fechaNac))   Date
+            med.setFechaNac(fechaNac);
+            // if(buscarCambios(genero))    Enum
+                med.setGenero(genero);
+            if(buscarCambios(mail))
+                med.setMail(mail);
+            // if(buscarCambios(provincia)) Enum
+            med.setProvincia(provincia);
+            if(buscarCambios(ciudad))
+                med.setCiudad(ciudad);
+            if(buscarCambios(otros))
+                med.setOtros(otros);
+            if(buscarCambios(clave))
+                med.setClave(clave);
+            if(buscarCambios(especialidad1))
+                med.setEspecialidad1(especialidad1);
+            if(buscarCambios(especialidad2))
+                med.setEspecialidad1(especialidad2);
+            if(buscarCambios(especialidad3))
+                med.setEspecialidad1(especialidad3);
+            // if(buscarCambios(centrosMedicos))    List<int>
+            med.setCentrosMedicos(centrosMedicos);
+           
+            medRep.save(med);
+        } else {
+            throw new Errores("No se encuentra el médico de matricula " + matricula + " en la base de datos");
+        }
+    }
+    
+    public void eliminar(Integer matricula) throws Errores {
+        Optional<medico> medOpt = medRep.findById(matricula);
+        if(medOpt.isPresent()){
+            medRep.deleteById(matricula);
+        } else {
+            throw new Errores("No se encuentra el médico de matricula " + matricula + " en la base de datos");
+        }
+
+    }
+    
+    private Boolean buscarCambios(String text) {
+        return text == null || text.isEmpty();
     }
     
 }
