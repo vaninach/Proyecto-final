be99cd972a67580c847e0d28a8bfda3b7db8963a
Vanina Chiarpotti <vani.chiarpotti@gmail.com>
2021-11-10 21:45:55 -0300

    commit para mia

--- src/main/java/alto/grupo/servicios/pacienteSe.java
+++ src/main/java/alto/grupo/servicios/pacienteSe.java
@@ -9,6 +9,7 @@ import alto.grupo.entidades.paciente;
 import alto.grupo.enums.Genero;
 import alto.grupo.enums.GrupoS;
 import alto.grupo.enums.Provincia;
+import alto.grupo.errores.Errores;
 import alto.grupo.repositorios.pacienteRep;
 import java.util.Date;
 import java.util.Optional;
@@ -24,62 +25,88 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class pacienteSe implements UserDetailsService {
-    
+
     @Autowired
     pacienteRep parep;
-    
-    public void Crearpaciente(String DNI, String nombre, String apellido, Date fechaNac, Genero genero, String estadoCivil, String telefono, String mail, String nombreContacto, String telefonoContacto, GrupoS grupoS, String obraS1, String nAfiliadoOS1, String obraS2, String nAfiliadoOS2, String obraS3, String nAfiliadoOS3, String nacionalidad, Provincia provincia, String ciudad, String calle, String numero, String piso, String departamento, String otros, String clave) throws Exception{
-        Optional<paciente> paci=parep.findById(DNI);
-        if(!paci.isPresent()){
-            paciente pac=new paciente();
-            pac.setDNI(DNI);
-            pac.setNombre(nombre);
-            pac.setApellido(apellido);
-            pac.setFechaNac(fechaNac);
-            pac.setGenero(genero);
-            pac.setEstadoCivil(estadoCivil);
-            pac.setTelefono(telefono);
-            pac.setMail(mail);
-            pac.setNombreContacto(nombreContacto);
-            pac.setTelefono(telefono);
-            pac.setGrupoS(grupoS);
-            pac.setObraS1(obraS1);
-            pac.setnAfiliadoOS1(nAfiliadoOS1);
-            pac.setObraS1(obraS2);
-            pac.setnAfiliadoOS1(nAfiliadoOS2);
-            pac.setObraS1(obraS3);
-            pac.setnAfiliadoOS1(nAfiliadoOS3);
-            pac.setNacionalidad(nacionalidad);
-            pac.setProvincia(provincia);
-            pac.setCiudad(ciudad);
-            pac.setCalle(calle);
-            pac.setNumero(numero);
-            pac.setPiso(piso);
-            pac.setDepartamento(departamento);
-            pac.setOtros(otros);
-            pac.setClave(clave);
-            
-            
-            parep.save(pac);
+
+    public void Crearpaciente(String DNI, String nombre, String apellido, Date fechaNac, Genero genero, String estadoCivil, String telefono, String mail, String nombreContacto, String telefonoContacto, GrupoS grupoS, String obraS1, String nAfiliadoOS1, String obraS2, String nAfiliadoOS2, String obraS3, String nAfiliadoOS3, String nacionalidad, Provincia provincia, String ciudad, String calle, String numero, String piso, String departamento, String otros, String clave) throws Errores {
+        Optional<paciente> paci = parep.findById(DNI);
+        if (!paci.isPresent()) {
+
+            try {
+                
+                validar(DNI);
+                validar(nombre);
+                validar(apellido);
+//                validar(fechaNac); // esta validacion??
+                validar(estadoCivil);
+                validar(telefono);
+                validar(mail);
+                validar(nombreContacto);
+                validar(telefonoContacto);
+                validar(obraS1);
+                validar(nAfiliadoOS1);
+                validar(obraS2);
+                validar(nAfiliadoOS2);
+                validar(obraS3);
+                validar(nAfiliadoOS3);
+                validar(nacionalidad);
+                validar(ciudad);
+                validar(calle);
+                validar(numero);
+                validar(piso);
+                validar(departamento);
+                validar(otros);
+                validar(clave);
+                
+                paciente pac = new paciente();
+                pac.setDNI(DNI);
+                pac.setNombre(nombre);
+                pac.setApellido(apellido);
+                pac.setFechaNac(fechaNac);
+                pac.setGenero(genero);
+                pac.setEstadoCivil(estadoCivil);
+                pac.setTelefono(telefono);
+                pac.setMail(mail);
+                pac.setNombreContacto(nombreContacto);
+                pac.setTelefono(telefono);
+                pac.setGrupoS(grupoS);
+                pac.setObraS1(obraS1);
+                pac.setnAfiliadoOS1(nAfiliadoOS1);
+                pac.setObraS1(obraS2);
+                pac.setnAfiliadoOS1(nAfiliadoOS2);
+                pac.setObraS1(obraS3);
+                pac.setnAfiliadoOS1(nAfiliadoOS3);
+                pac.setNacionalidad(nacionalidad);
+                pac.setProvincia(provincia);
+                pac.setCiudad(ciudad);
+                pac.setCalle(calle);
+                pac.setNumero(numero);
+                pac.setPiso(piso);
+                pac.setDepartamento(departamento);
+                pac.setOtros(otros);
+                pac.setClave(clave);
+
+                parep.save(pac);
+
+            } catch (Errores e) {
+                System.out.println(e);
+            }
+        } else {
+            throw new Errores("El paciente ya tiene una cuenta");
         }
-        
-        else{
-            throw new Exception();
-        }
-        
-        
+
         return;
-        
+
     }
-    
-    private void validar(String nombre) throws ErrorServicio {
+
+    private void validar(String nombre) throws Errores {
         if (nombre == null || nombre.isEmpty()) {
-            throw new ErrorServicio("el dato " + nombre + "no es valido");
+            throw new Errores("el dato " + nombre + "no es valido");
         }
 
     }
-    
-    
+
 //     @Override
 //    public UserDetails loadUserByUsername(String usuario) throws UsernameNotFoundException {
 //        paciente pac = rec.BuscarPorUsuario(usuario);
@@ -102,13 +129,9 @@ public class pacienteSe implements UserDetailsService {
 //            return null;
 //        }
 //    }
-
     @Override
     public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-   
-    
-    
+
 }
