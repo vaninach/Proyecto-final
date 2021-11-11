94cbfdb5c1de07d1d571641004594bb19c494570
Vanina Chiarpotti <vani.chiarpotti@gmail.com>
2021-11-11 20:12:49 -0300

    commit servicio paciente terminado con las funciones basicas

--- src/main/java/alto/grupo/servicios/pacienteSe.java
+++ src/main/java/alto/grupo/servicios/pacienteSe.java
@@ -34,11 +34,11 @@ public class pacienteSe implements UserDetailsService {
         if (!paci.isPresent()) {
 
             try {
-                
+
                 validar(DNI);
                 validar(nombre);
                 validar(apellido);
-//                validar(fechaNac); // esta validacion??
+//                validar(fechaNac); // esta validacion?? no haria falta porque aparece el calensaario?
                 validar(estadoCivil);
                 validar(telefono);
                 validar(mail);
@@ -58,7 +58,7 @@ public class pacienteSe implements UserDetailsService {
                 validar(departamento);
                 validar(otros);
                 validar(clave);
-                
+
                 paciente pac = new paciente();
                 pac.setDNI(DNI);
                 pac.setNombre(nombre);
@@ -100,10 +100,100 @@ public class pacienteSe implements UserDetailsService {
 
     }
 
+    private void Modificar(String DNI, String nombre, String apellido, Date fechaNac, Genero genero, String estadoCivil, String telefono, String mail, String nombreContacto, String telefonoContacto, GrupoS grupoS, String obraS1, String nAfiliadoOS1, String obraS2, String nAfiliadoOS2, String obraS3, String nAfiliadoOS3, String nacionalidad, Provincia provincia, String ciudad, String calle, String numero, String piso, String departamento, String otros, String clave) throws Errores {
+
+        Optional<paciente> paciop = parep.findById(DNI);
+        if (paciop.isPresent()) {
+
+                paciente pac=paciop.get();
+
+                if(BuscarCambios(DNI))
+                    pac.setDNI(DNI);
+                if(BuscarCambios(nombre))
+                    pac.setNombre(nombre);
+                if(BuscarCambios(apellido))
+                    pac.setApellido(apellido);
+           //     if(BuscarCambios(fechaNac))  //ver esta validacion
+                pac.setFechaNac(fechaNac);
+           //     if(BuscarCambios(genero))     //ver esta validacion
+                pac.setGenero(genero);
+                if(BuscarCambios(estadoCivil))
+                    pac.setEstadoCivil(estadoCivil);
+                if(BuscarCambios(telefono))
+                    pac.setTelefono(telefono);
+                if(BuscarCambios(mail))
+                    pac.setMail(mail);
+                if(BuscarCambios(nombreContacto))
+                    pac.setNombreContacto(nombreContacto);
+                if(BuscarCambios(telefono))
+                    pac.setTelefono(telefono);
+              //  if(BuscarCambios(grupoS))     //ver esta validacion
+                pac.setGrupoS(grupoS);
+                if(BuscarCambios(obraS1))
+                    pac.setObraS1(obraS1);
+                if(BuscarCambios(nAfiliadoOS1))
+                    pac.setnAfiliadoOS1(nAfiliadoOS1);
+                if(BuscarCambios(obraS2))
+                    pac.setObraS1(obraS2);
+                if(BuscarCambios(nAfiliadoOS2))
+                    pac.setnAfiliadoOS1(nAfiliadoOS2);
+                if(BuscarCambios(obraS3))
+                    pac.setObraS1(obraS3);
+                if(BuscarCambios(nAfiliadoOS3))
+                    pac.setnAfiliadoOS1(nAfiliadoOS3);
+                if(BuscarCambios(nacionalidad))
+                    pac.setNacionalidad(nacionalidad);
+              //  if(BuscarCambios(provincia))  // ver esta validacion
+                pac.setProvincia(provincia);
+                if(BuscarCambios(ciudad))
+                    pac.setCiudad(ciudad);
+                if(BuscarCambios(calle))
+                    pac.setCalle(calle);
+                if(BuscarCambios(numero))
+                    pac.setNumero(numero);
+                if(BuscarCambios(piso))
+                    pac.setPiso(piso);
+                if(BuscarCambios(departamento))
+                    pac.setDepartamento(departamento);
+                if(BuscarCambios(otros))
+                    pac.setOtros(otros);
+                if(BuscarCambios(clave))
+                    pac.setClave(clave);
+
+                parep.save(pac);
+
+           
+        } else {
+            throw new Errores("El paciente no exite en la base de datos");
+        }
+
+
+        
+        
+    }
+
+    private void Eliminar(String DNI) throws Errores {
+        Optional<paciente> paci = parep.findById(DNI);
+        if (paci.isPresent()) {
+            parep.deleteById(DNI);
+        } else {
+            throw new Errores("No se encontro el paciente solicitado");
+        }
+
+    }
+
     private void validar(String nombre) throws Errores {
         if (nombre == null || nombre.isEmpty()) {
-            throw new Errores("el dato " + nombre + "no es valido");
+            throw new Errores("El dato " + nombre + "no es valido");
         }
+
+    }
+    
+    private Boolean BuscarCambios(String nombre) {
+        if (nombre == null && nombre.isEmpty()) {
+            return false;
+        }
+        else return true;
 
     }
 
