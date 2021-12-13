d26cf45fa6c60777ed98176775a4dd5d08b61aee
vaninach <vani.chiarpotti@gmail.com>
2021-12-13 13:09:29 -0300

    url paciente y logout

--- src/main/java/alto/grupo/controladores/inicioController.java
+++ src/main/java/alto/grupo/controladores/inicioController.java
@@ -5,8 +5,14 @@
  */
 package alto.grupo.controladores;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -22,5 +28,21 @@ public class inicioController {
 	public String login() {
 		return "inicio.html";
 	}
+        
+        
+          @RequestMapping(value = {"/logout"}, method = RequestMethod.POST)
+    public String logoutDo(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        SecurityContextHolder.clearContext();
+        session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        for (Cookie cookie : request.getCookies()) {
+            cookie.setMaxAge(0);
+        }
+
+        return "redirect:/inicio";
+    }
     
 }
