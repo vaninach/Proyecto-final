e19607c727324be1aca5b376f0504953a3227c92
Vanina Chiarpotti <vani.chiarpotti@gmail.com>
2021-12-14 20:38:29 -0300

    vincular medicos a centros medicos

--- src/main/java/alto/grupo/controladores/MedicoController.java
+++ src/main/java/alto/grupo/controladores/MedicoController.java
@@ -9,20 +9,15 @@ import alto.grupo.entidades.CentroMedico;
 import alto.grupo.entidades.HistoriasClinicas;
 import alto.grupo.entidades.Medico;
 import alto.grupo.entidades.Paciente;
-import alto.grupo.enums.Genero;
-import alto.grupo.enums.Provincia;
 import alto.grupo.errores.Errores;
 import alto.grupo.servicios.CentroMedicoSe;
 import alto.grupo.servicios.HistClinicaSe;
 import alto.grupo.servicios.MedicoSe;
 import alto.grupo.servicios.PacienteSe;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -379,10 +374,8 @@ public class MedicoController {
     }
 
     @PostMapping("/Medico/VincularCM")
-    public String resultadosBusquedaPaciente(HttpSession session, Model model, @RequestParam Long RegistroCM) throws Errores{
+    public String resultadosBusquedaPaciente(HttpSession session, Model model, @RequestParam Long RegistroCM){
         
-        CentroMedico cmed=centromedicoSe.buscarPorCodigo(RegistroCM);
-        List<CentroMedico> listaCM=new ArrayList<>();
         
         
 
@@ -394,9 +387,15 @@ public class MedicoController {
             return "redirect:/inicio";
         }
         
-       if(cmed!=null){
+       try{
+           CentroMedico cmed=centromedicoSe.buscarPorCodigo(RegistroCM);
+        List<CentroMedico> listaCM=new ArrayList<>();
+        
            listaCM.add(cmed);
            model.addAttribute("listaCM", listaCM);
+       }
+       catch(Errores er){
+           model.addAttribute("mensaje", er.getMessage());
        }
        
         
